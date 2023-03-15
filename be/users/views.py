af1055c30d5186bbbfce5349cf830df2ef060290
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2023-03-15 01:36:11 -0400

    added login functionallity

--- be/users/views.py
+++ be/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from submissions.models import Submission, Contest, Genre
 from submissions.forms import SubmissionForm
+from .forms import UserRegistrationForm
 
 def submission_list(request):
     submissions = Submission.objects.all()
@@ -23,3 +26,34 @@ def submission_create(request):
     else:
         form = SubmissionForm()
     return render(request, 'submissions/submission_form.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('submissions:submission_list')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('submissions:submission_list')
+        else:
+            messages.error(request, 'There was an error with your registration.')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
