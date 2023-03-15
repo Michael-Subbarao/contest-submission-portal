174ec255e9b415254d22bca1d54064ea90b349eb
Michael-Subbarao <53024176+Michael-Subbarao@users.noreply.github.com>
2023-03-15 00:47:28 -0400

    added url routing

--- be/submissions/urls.py
+++ be/submissions/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'submissions'
+
 urlpatterns = [
-    # path('', views.submission_list, name='submission_list'),
+    path('', views.submission_list, name='submission_list'),
+    path('<int:pk>/', views.submission_detail, name='submission_detail'),
+    path('new/', views.submission_create, name='submission_create'),
 ]
